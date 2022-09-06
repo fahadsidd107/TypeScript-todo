86099632e53078e194c18a1e88acb45b076a7e1c
FadFlex <fsiddiqui107@gmail.com>
2022-09-06 10:19:38 +0500

    work

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import React, { FC, ChangeEvent, useState } from "react";
+import "./App.css";
+import { ITask } from "./Interfaces";
+import Task from './Components/Task'
+
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDealine] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDealine(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDealine(0);
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in Days)..."
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+<div className="todoList ">
+  {todoList.map((task : ITask , key : number)=>{
+    return(
+      <Task key={key} task={task}/>
+    )
+  })}
+</div>
+    </div>
+  );
+};
+
+export default App;
